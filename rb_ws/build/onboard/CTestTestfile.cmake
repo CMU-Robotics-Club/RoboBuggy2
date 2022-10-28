2abf11dcf5128fc35da0b5bcb445dd00370b4929
Christian Luu <christianvluu@gmail.com>
2022-10-27 17:22:25 -0700

    Revert "revert build folder upload"
    This reverts commit 644ff8a43d9a55ac882dbb846126224d2a4714e6.

---
+++ rb_ws/build/onboard/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /rb_ws/src/onboard
+# Build directory: /rb_ws/build/onboard
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
