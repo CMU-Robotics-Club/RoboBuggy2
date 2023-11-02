f358928f85e4f49053af3d1f32dd9630a3f77a62
PatXue <95881915+PatXue@users.noreply.github.com>
2023-11-02 16:40:50 -0400

    Renamed VelocityUpdater attributes

--- rb_ws/src/buggy/scripts/2d_sim/velocity_updater.py
+++ rb_ws/src/buggy/scripts/2d_sim/velocity_updater.py
@@ -1,29 +1,34 @@
 #! /usr/bin/env python3
 import rospy
 from std_msgs.msg import Float64
+import threading
 
 class VelocityUpdater:
+    RATE = 100
+
     def __init__(self):
         self.vel_publisher = rospy.Publisher(
             "buggy/velocity", Float64, queue_size=1
         )
-        self.velocity: float = 15.0
-        self.accel: float = 0.1 # Dummy value for acceleration
+        self.buggy_vel = 0.0
         
         self.rate = 100
+        self.lock = threading.Lock()
 
     def tick(self):
         new_velocity = Float64()
-        new_velocity.data = self.velocity + self.accel / self.rate
+        new_velocity.data = self.buggy_vel + accel / self.RATE
 
-        self.velocity = new_velocity.data
+        self.buggy_vel = new_velocity.data
         self.vel_publisher.publish(new_velocity)
+        
+        self.time += 1 / self.RATE
+
 
 if __name__ == "__main__":
     rospy.init_node("velocity_updater")
     vel = VelocityUpdater()
-    rate = rospy.Rate(100)
+    rate = rospy.Rate(vel.RATE)
 
     while not rospy.is_shutdown():
         vel.tick()
-        rate.sleep()
