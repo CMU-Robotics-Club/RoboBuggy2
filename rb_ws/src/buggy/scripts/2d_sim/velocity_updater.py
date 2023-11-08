cc17350625744da390b1f1736e1bfa52fec1c9c3
PatXue <95881915+PatXue@users.noreply.github.com>
2023-11-07 19:08:19 -0500

    Change to set buggy velocity through Controller

--- rb_ws/src/buggy/scripts/2d_sim/velocity_updater.py
+++ rb_ws/src/buggy/scripts/2d_sim/velocity_updater.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python3
 import rospy
+from controller_2d import Controller
 from std_msgs.msg import Float64
 import threading
 
@@ -7,28 +8,22 @@ class VelocityUpdater:
     RATE = 100
 
     def __init__(self):
-        self.vel_publisher = rospy.Publisher(
-            "buggy/velocity", Float64, queue_size=1
-        )
         self.buggy_vel = 0.0
-        
         self.time = 0.0
 
         self.lock = threading.Lock()
 
     def calculate_accel(self) -> float:
         if self.time < 20.0: return 1.0
-        else: return 0.0
+        else: return -1.0
 
     def tick(self):
         accel = self.calculate_accel()
 
-        new_velocity = Float64()
-        new_velocity.data = self.buggy_vel + accel / self.RATE
+        new_velocity = self.buggy_vel + accel / self.RATE
+        self.buggy_vel = new_velocity
+        self.controller.set_velocity(new_velocity)
 
-        self.buggy_vel = new_velocity.data
-        self.vel_publisher.publish(new_velocity)
-        
         self.time += 1 / self.RATE
 
 
