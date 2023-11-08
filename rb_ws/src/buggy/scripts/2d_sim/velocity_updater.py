d4fd0dcd3b5ad79ebacbb0655f7b1603e3863f2d
PatXue <95881915+PatXue@users.noreply.github.com>
2023-11-07 19:10:02 -0500

    Add subscriber to access position of buggy

--- rb_ws/src/buggy/scripts/2d_sim/velocity_updater.py
+++ rb_ws/src/buggy/scripts/2d_sim/velocity_updater.py
@@ -2,16 +2,28 @@
 import rospy
 from controller_2d import Controller
 from std_msgs.msg import Float64
+from geometry_msgs.msg import Pose
 import threading
 
 class VelocityUpdater:
     RATE = 100
 
     def __init__(self):
+        self.pose_subscriber = rospy.Subscriber(
+            "sim_2d/utm", Pose, self.update_position
+        )
+
         self.buggy_vel = 0.0
         self.time = 0.0
 
+        self.controller = Controller()
+
         self.lock = threading.Lock()
+    
+    def update_position(self, new_pose: Pose):
+        '''Callback function to update internal position variable'''
+        with self.lock:
+            self.position = new_pose.position
 
     def calculate_accel(self) -> float:
         if self.time < 20.0: return 1.0
