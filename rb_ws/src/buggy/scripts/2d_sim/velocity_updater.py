b59f2d60772cb8dd83f694b5a4722c1bbc77663f
PatXue <95881915+PatXue@users.noreply.github.com>
2023-11-12 22:09:09 -0500

    Add position-based acceleration

--- rb_ws/src/buggy/scripts/2d_sim/velocity_updater.py
+++ rb_ws/src/buggy/scripts/2d_sim/velocity_updater.py
@@ -3,18 +3,29 @@ import rospy
 from controller_2d import Controller
 from std_msgs.msg import Float64
 from geometry_msgs.msg import Pose
+from geometry_msgs.msg import Point
 import threading
+import math
 
 class VelocityUpdater:
     RATE = 100
+    CHECKPOINTS = [
+        (589701, 4477160, 20, 0.5)
+    ]
 
     def __init__(self):
         self.pose_subscriber = rospy.Subscriber(
             "sim_2d/utm", Pose, self.update_position
         )
 
-        self.buggy_vel = 0.0
+        self.buggy_vel = 15.0
         self.time = 0.0
+        self.accel = 0.0
+
+        self.position = Point()
+        self.position.x = 0
+        self.position.y = 0
+        self.position.z = 0
 
         self.controller = Controller()
 
@@ -25,14 +36,16 @@ class VelocityUpdater:
         with self.lock:
             self.position = new_pose.position
 
-    def calculate_accel(self) -> float:
-        if self.time < 20.0: return 1.0
-        else: return -1.0
+    def calculate_accel(self):
+        for (x,y,r,a) in self.CHECKPOINTS:
+            dist = math.sqrt((x - self.position.x)**2 + (y - self.position.y)**2)
+            if dist < r:
+                self.accel = a
 
-    def tick(self):
-        accel = self.calculate_accel()
+    def step(self):
+        self.calculate_accel()
 
-        new_velocity = self.buggy_vel + accel / self.RATE
+        new_velocity = self.buggy_vel + self.accel / self.RATE
         self.buggy_vel = new_velocity
         self.controller.set_velocity(new_velocity)
 
@@ -45,5 +58,5 @@ if __name__ == "__main__":
     rate = rospy.Rate(vel.RATE)
 
     while not rospy.is_shutdown():
-        vel.tick()
+        vel.step()
         rate.sleep()
