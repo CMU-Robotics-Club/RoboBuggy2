827b92b2bd5b69979fc576f4f88defff2a35b2bb
NoRePercussions <tucker@tuckershea.com>
2023-11-28 12:00:23 -0500

    Add heartbeat loop

--- rb_ws/src/buggy/scripts/auton/heartbeat.py
+++ rb_ws/src/buggy/scripts/auton/heartbeat.py
@@ -2,11 +2,12 @@ import rospy
 from rospy.std_msgs import Int64
 
 class Heartbeat:
-    RATE_HZ: float = 10
+    rate: rospy.Rate
     heartbeat_num: int
     heartbeat_publisher: rospy.Publisher
 
-    def __init__(self, buggy_name: str):
+    def __init__(self):
+        rate = rospy.Rate(10)  # rate in hz
         heartbeat_num = 0
         heartbeat_publisher = rospy.Publisher(
             "/heartbeat",
@@ -17,6 +18,12 @@ class Heartbeat:
         self.heartbeat_publisher.publish(Int64(self.heartbeat_num))
         self.heartbeat_num += 1
 
+    def start(self):
+        while not rospy.is_shut_down():
+            self.update_heartbeat()
+            self.rate.sleep()
+
 
 if __name__ == "__main__":
-    pass
+    hb_publisher = Heartbeat()
+    hb_publisher.start()
