f8413a3f340e191853b52c3037b7b932dc481e56
Buggy <buggy@buggy.com>
2024-02-16 16:07:44 -0500

    Added debug steer routine

---
+++ rb_ws/src/buggy/scripts/debug/debug_steer.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+from abc import ABC, abstractmethod
+
+import rospy
+from std_msgs.msg import Float64
+
+
+class DebugController():
+
+    """
+    Debug Controller
+    Sends oscillating steering command for firmware debug
+    """
+
+    def __init__(self) -> None:
+        self.steer_publisher = rospy.Publisher(
+        "buggy/input/steering", Float64, queue_size=1)
+        self.rate = 100
+
+    def loop(self):
+        rate = rospy.Rate(self.rate)
+        tick_count = 0
+        steer_cmd = 0
+        add = True
+
+        while not rospy.is_shutdown():
+            if (add):
+                steer_cmd += 18 / 100
+            else:
+                steer_cmd -= 18 / 100 
+            
+            self.steer_publisher.publish(Float64(steer_cmd))
+
+            tick_count += 1
+            if (tick_count == 200):
+                tick_count = 0
+
+            if (steer_cmd >= 18):
+                add = False
+            
+            if (steer_cmd <= -18):
+                add = True
+            rate.sleep()
+
+if __name__ == "__main__":
+    rospy.init_node("debug_steer")
+    d = DebugController()
+    d.loop()
