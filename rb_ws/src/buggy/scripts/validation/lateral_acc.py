5ba239c64442bb77fa33dac65eff7581e1b11e36
sam <samriddhi.bhardwaj@gmail.com>
2024-02-18 18:05:20 -0500

    fixed tiny absolute value issue

--- rb_ws/src/buggy/scripts/validation/lateral_acc.py
+++ rb_ws/src/buggy/scripts/validation/lateral_acc.py
@@ -21,9 +21,9 @@ class Later_acc:
             self.radius_front_wheel = 1.104 / np.sin(np.deg2rad(self.steering))
             self.radius_rear_wheel = 1.104 / np.tan(np.deg2rad(self.steering))
 
-        self.flag_front= 0.4
-        self.flag_rear= 0.5
-        self.flag_avg= 0.5
+        self.thres_front = 0.4
+        self.thres_rear = 0.5
+        self.thres_avg = 0.5
 
     def update_velocity(self,new_vel):
         self.velocity = new_vel
@@ -42,16 +42,16 @@ class Later_acc:
 
             time_rn = time.time() - start_time
             
-            front_flag_val = 0
-            rear_flag_val = 0
-            avg_flag_val = 0
-            if lat_accel_front_wheel > self.flag_front:
-                front_flag_val = 1
-            if lat_accel_rear_wheel > self.flag_rear:
-                rear_flag_val = 1
-            if lat_accel > self.flag_avg:
-                avg_flag_val = 1
-            writer.writerow([time_rn, lat_accel_front_wheel,lat_accel_rear_wheel,lat_accel,front_flag_val,rear_flag_val,avg_flag_val])
+            front_thresh_val = 0
+            rear_thresh_val = 0
+            avg_thresh_val = 0
+            if lat_accel_front_wheel > np.absolute(self.thresh_front):
+                front_thresh_val = 1
+            if lat_accel_rear_wheel > np.absolute(self.thresh_rear):
+                rear_thresh_val = 1
+            if lat_accel > np.absolute(self.thresh_avg):
+                avg_thresh_val = 1
+            writer.writerow([time_rn, lat_accel_front_wheel,lat_accel_rear_wheel,lat_accel,front_thresh_val,rear_thresh_val,avg_thresh_val])
 
             print("lateral acc is ", lat_accel)
             print("time is ",time_rn)
