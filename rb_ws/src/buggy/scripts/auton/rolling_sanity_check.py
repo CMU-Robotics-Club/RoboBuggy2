dcb4be90cb4f0ec0be78e5e25acd400c135c5f0b
TiaSinghania <samanvita.singhania@gmail.com>
2024-03-15 00:23:16 -0400

    fixed import errors

--- rb_ws/src/buggy/scripts/auton/rolling_sanity_check.py
+++ rb_ws/src/buggy/scripts/auton/rolling_sanity_check.py
@@ -1,18 +1,20 @@
+#! /usr/bin/env python3
+
 import numpy as np
 import sys
 from nav_msgs.msg import Odometry
-from rb_ws.src.buggy.scripts.auton.pose import Pose
-from rb_ws.src.buggy.scripts.auton.world import World
-from std_msgs.msg import Float32, Float64, Bool, Int8, String, IntList
+from pose import Pose
+from world import World
+from std_msgs.msg import Float32, Float64, Bool, Int8, String, Int8MultiArray
 
 import rospy
 
 from brake_controller import BrakeController
-from auton.controller import Controller
+from controller import Controller
 from trajectory import Trajectory
-from microstrain_inertial_msgs.msg import FilterStatus, MipFilterStatusGq7StatusFlags
-from microstrain_inertial_msgs.msg import SensorOverrangeStatus
-from sensor_msgs import Imu
+from microstrain_inertial_msgs.msg import FilterStatus
+from microstrain_inertial_msgs.msg import ImuOverrangeStatus
+from sensor_msgs.msg import Imu
 
 
 class SanityCheck:
@@ -35,8 +37,8 @@ class SanityCheck:
 
 
         rospy.Subscriber(self_name + "/nav/odom", Odometry, self.update_self_odom)
-        rospy.Subscriber(self_name + "/imu/overrange_status", SensorOverrangeStatus, self.update_overrange_status)
-        rospy.Subscriber(self_name + "/imu/data", Imu, self.update_filter_location)
+        rospy.Subscriber(self_name + "/imu/overrange_status", ImuOverrangeStatus, self.update_overrange_status)
+        rospy.Subscriber(self_name + "/nav/filtered_imu/data", Imu, self.update_filter_location)
 
         rospy.Subscriber(self_name + "/nav/status.status_flags", FilterStatus, self.update_status_flags)
 
@@ -52,7 +54,7 @@ class SanityCheck:
         )
 
         self.status_flags_publisher = rospy.Publisher(
-            self_name + "/nav/status/tripped_status_flags", IntList, queue_size=1
+            self_name + "/nav/status/tripped_status_flags", Int8MultiArray, queue_size=1
         )
 
     def update_self_odom(self, msg):
@@ -61,7 +63,7 @@ class SanityCheck:
     def update_other_odom(self, msg):
         self.other_odom_msg = msg
 
-    def update_overrange_status(self, msg : SensorOverrangeStatus):
+    def update_overrange_status(self, msg : ImuOverrangeStatus):
         self.imu_overrange_status = msg.status
 
     def update_status_flags(self, msg : FilterStatus):
@@ -69,34 +71,43 @@ class SanityCheck:
 
     def update_filter_location(self, msg : Imu):
         #TODO: what data should we store/ use to compare filter location
-        self.filter_location = msg.orientation.x
+        if (self.filter_location != None):
+            self.filter_location = msg.orientation.x
 
 
     def calc_covariance(self):
-        self.covariance = self.self_odom_msg.pose.covariance[0] ** 2 + self.self_odom_msg.pose.covariance[7] ** 2
-        # publishes true if covariance is good
-        self.covariance_status_publisher(self.covariance <= 1**2)
+        if (self.self_odom_msg == None):
+            self.covariance_status_publisher.publish(False)
+        else:
+            self.covariance_status_publisher.publish(self.self_odom_msg.pose.covariance[0] ** 2 + self.self_odom_msg.pose.covariance[7] ** 2 <= 1**2)
 
     def calc_locations(self):
         #TODO: actually publish relevant data
-        current_rospose = self.self_odom_msg.pose.pose
-        pose_gps = Pose.rospose_to_pose(current_rospose)
-        self.location = World.gps_to_world_pose(pose_gps)
+        if (self.self_odom_msg == None or self.location == None):
+            self.filter_gps_status_publisher.publish(False)
+        else:
+            current_rospose = self.self_odom_msg.pose.pose
+            pose_gps = Pose.rospose_to_pose(current_rospose)
+            self.location = World.gps_to_world_pose(pose_gps)
 
-        # self.filter_gps_status_publisher(abs(self.filter_location - self.location) < 0.5)
+            # self.filter_gps_status_publisher.publish(abs(self.filter_location - self.location) < 0.5)
 
     def is_overrange (self):
         s = self.imu_overrange_status
-        accel_status = s.status_accel_x or s.status_accel_y or s.status_accel_z
-        gyro_status = s.status_gyro_x or s.status_gyro_y or s.status_gyro_z
-        mag_status = s.status_mag_x or s.status_mag_y or s.status_mag_z
+        if (s == None):
+            self.overrange_status_publisher.publish(False)
+        else:
+            accel_status = s.status_accel_x or s.status_accel_y or s.status_accel_z
+            gyro_status = s.status_gyro_x or s.status_gyro_y or s.status_gyro_z
+            mag_status = s.status_mag_x or s.status_mag_y or s.status_mag_z
 
-        # publishes true if ALL flags are FALSE (if the imu looks good)
-        self.imu_overrange_status(not (accel_status or gyro_status or mag_status or s.status_press))
+            # publishes true if ALL flags are FALSE (if the imu looks good)
+            self.imu_overrange_status.publish(not (accel_status or gyro_status or mag_status or s.status_press))
 
-    def filter_status_warning (self, publishers):
+    def filter_status_warning (self):
         b = bin(self.status_flag_val)
-        self.flags = []
+        self.flags = Int8MultiArray()
+        self.flags.data = []
         error_message = ""
         for i in range (len(b)):
             if (b[i] == '1'):
@@ -104,16 +115,16 @@ class SanityCheck:
                 error_message += self.error_messages[i] + " "
 
         # publish bit values
-        publishers[0].publish(self.flags)
+        self.status_flags_publisher.publish(self.flags)
 
         # publish string translations
-        publishers[1].publish(error_message)
+        self.error_message_publisher.publish(error_message)
 
     def sanity_check(self):
         self.calc_covariance()
         self.calc_locations()
         self.is_overrange()
-        self.filter_status_warning([self.error_message_publisher, self.status_flags_publisher])
+        self.filter_status_warning()
 
 if __name__ == "__main__":
     rospy.init_node("rolling_sanity_check")
