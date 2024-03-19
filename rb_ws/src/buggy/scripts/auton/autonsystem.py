31394ede424204c6729cdbd260b9956baa0a45a7
TiaSinghania <samanvita.singhania@gmail.com>
2024-03-19 12:12:03 -0400

    trailing whitespace

--- rb_ws/src/buggy/scripts/auton/autonsystem.py
+++ rb_ws/src/buggy/scripts/auton/autonsystem.py
@@ -156,7 +156,6 @@ class AutonSystem:
             rospy.sleep(0.001)
         print("done checking initialization status")
         self.init_check_publisher.publish(True)
-        
         # initialize global trajectory index
 
         with self.lock:
