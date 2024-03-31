2095c00e733b867d8e8321a1a1ac9eff776c3ebb
sam <samriddhi.bhardwaj@gmail.com>
2024-03-31 17:12:18 -0400

    script to log lateral acceleration

--- rb_ws/src/buggy/scripts/validation/lateral_acc.py
+++ rb_ws/src/buggy/scripts/validation/lateral_acc.py
@@ -6,42 +6,45 @@ import numpy as np
 import csv
 import time
 
-# column that tells you if you're acceleration above some value
-file = open('src/buggy/scripts/validation/lateral_acc.txt', 'w', newline='')
+file = open("/rb_ws/src/buggy/scripts/validation/lateral_acc.txt", 'w', newline='')
 writer = csv.writer(file)
-writer.writerow(["time", "lateral accel front","lateral accel rear","average","flag front","flag rear","flag avg"])
+writer.writerow(["time", "lateral accel front","lateral accel rear","average","flag front","flag rear"])
 
-class Later_acc:
+class LateralAcc:
   
-    def __init__(self, velocity, steering_angle, acc):
-        self.velocity = velocity
-        self.steering = steering_angle
-        self.acc = 0
-        if self.steering!=0:
-            self.radius_front_wheel = 1.104 / np.sin(np.deg2rad(self.steering))
-            self.radius_rear_wheel = 1.104 / np.tan(np.deg2rad(self.steering))
+    def __init__(self):
+        self.velocity = 0
+        self.steering = 0
+        self.wheelbase = 1.104
 
-        self.thres_front = 0.4
-        self.thres_rear = 0.5
-        self.thres_avg = 0.5
+        if self.steering != 0:
+            self.radius_front_wheel = self.wheelbase / np.sin(np.deg2rad(self.steering))
+            self.radius_rear_wheel = self.wheelbase / np.tan(np.deg2rad(self.steering))
+        # in m/s^2
+        self.thresh_front = 0.9*9.81
+        self.thresh_rear = 0.6*9.81
+        self.thresh_avg = 0.75*9.81
+        self.start_time = time.time()
+        rospy.Subscriber("/SC/velocity", Float64, self.callback_vel)
+        rospy.Subscriber("/SC/input/steering",Float64,self.callback_steer)
 
     def update_velocity(self,new_vel):
         self.velocity = new_vel
 
     def update_steering(self,new_steer):
         self.steering = new_steer
-        if self.steering!=0:
-            self.radius_front_wheel = 1.104 / np.sin(np.deg2rad(self.steering))
-            self.radius_rear_wheel = 1.104 / np.tan(np.deg2rad(self.steering))
+        if self.steering != 0:
+            self.radius_front_wheel = self.wheelbase / np.sin(np.deg2rad(self.steering))
+            self.radius_rear_wheel = self.wheelbase / np.tan(np.deg2rad(self.steering))
 
     def calculate_lateral_acc(self):
-        if self.steering!=0:
+        if self.steering != 0:
             lat_accel_front_wheel = (self.velocity**2)/self.radius_front_wheel / 9.81
             lat_accel_rear_wheel = (self.velocity**2)/self.radius_rear_wheel / 9.81
             lat_accel = (lat_accel_front_wheel + lat_accel_rear_wheel) / 2
 
-            time_rn = time.time() - start_time
-            
+            time_rn = time.time() - self.start_time
+            # flags, if values exceed thresholds write 1
             front_thresh_val = 0
             rear_thresh_val = 0
             avg_thresh_val = 0
@@ -51,33 +54,21 @@ class Later_acc:
                 rear_thresh_val = 1
             if lat_accel > np.absolute(self.thresh_avg):
                 avg_thresh_val = 1
+
             writer.writerow([time_rn, lat_accel_front_wheel,lat_accel_rear_wheel,lat_accel,front_thresh_val,rear_thresh_val,avg_thresh_val])
 
-            print("lateral acc is ", lat_accel)
-            print("time is ",time_rn)
+    def callback_vel(self, vel_data):
+        lateral_acc.update_velocity(vel_data.data)
+        lateral_acc.calculate_lateral_acc()
 
-def callback_vel(vel_data):
-    lateral_acc.update_velocity(vel_data.data)
-    lateral_acc.calculate_lateral_acc()
-
-def callback_steer(steer):
-    lateral_acc.update_steering(steer.data)
-    lateral_acc.calculate_lateral_acc()
-
-def vel_subscriber():
-    rospy.Subscriber("/SC/velocity", Float64, callback_vel)
-
-def steering_subscriber():
-    rospy.Subscriber("/SC/input/steering",Float64,callback_steer)
+    def callback_steer(self, steer):
+        lateral_acc.update_steering(steer.data)
+        lateral_acc.calculate_lateral_acc()
     
-lateral_acc = Later_acc(0,0,0)
-start_time = time.time()
-
 if __name__ == '__main__':
     try:
+        lateral_acc = LateralAcc()
         rospy.init_node("later_acc_vel")
-        vel_subscriber()
-        steering_subscriber()
         rospy.spin()
     except rospy.ROSInterruptException:
         pass
