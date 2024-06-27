9520c84a35da361af17f18703385261d8b092982
TiaSinghania <samanvita.singhania@gmail.com>
2024-06-26 23:45:08 -0700

    oops reverted too far

--- rb_ws/src/buggy/paths/rosbag_to_pose_csv.py
+++ rb_ws/src/buggy/paths/rosbag_to_pose_csv.py
@@ -1,12 +1,17 @@
 #! /usr/bin/env python3
-import rosbag
 import argparse
-import uuid
-import json
 import csv
+
+import rosbag
 from tf.transformations import euler_from_quaternion
 
+
 def main():
+    """
+    bag_file is an input that reads the path to the bag file
+    output_file is an argument that reads the path to the output file
+    subsample is the number of points to be selected to be converted to waypoints
+    """
     # Read in bag path from command line
     parser = argparse.ArgumentParser()
     parser.add_argument("bag_file", help="Path to bag file")
@@ -26,30 +31,35 @@ def main():
     i = 0
 
     # Loop through bag
-    for topic, msg, t in bag.read_messages(topics="/nav/odom"):
+    for _, msg, _ in bag.read_messages(topics="/nav/odom"):
         # Skip waypoints
         if i % args.subsample != 0:
             i += 1
             continue
         i += 1
 
+        # TODO: Check Orientation
         lat = msg.pose.pose.position.x
         lon = msg.pose.pose.position.y
         orientation_q = msg.pose.pose.orientation
 
-        orientation_list = [orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w]
-        (roll, pitch, yaw) = euler_from_quaternion (orientation_list)
-
+        orientation_list = [
+            orientation_q.x,
+            orientation_q.y,
+            orientation_q.z,
+            orientation_q.w,
+        ]
+        (_, _, yaw) = euler_from_quaternion(orientation_list)
 
         waypoints.append([str(lat), str(lon), str(yaw)])
 
     # Write to csv file
-    with open(args.output_file, 'w', newline='') as csvfile:
-        writer = csv.writer(csvfile, delimiter=',',
-                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    with open(args.output_file, "w", newline="") as csvfile:
+        writer = csv.writer(
+            csvfile, delimiter=",", quotechar="|", quoting=csv.QUOTE_MINIMAL
+        )
         for row in waypoints:
             writer.writerow(row)
-        
 
 
 if __name__ == "__main__":
